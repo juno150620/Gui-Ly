d18adad6223854cf5681469ffd20b91ce220bb7c
juno150620 <86794856+juno150620@users.noreply.github.com>
2021-07-02 00:20:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
     introTitle: 'Công chúa à!',
-    introDesc: `Trên thế giới hơn 7 tỉ người
-    Chúng ta may mắn mà gặp được nhau
-    Có lẽ đó là 1 cái duyên`,
+    introDesc: `Trái đất có 7 tỉ người
+    Mà sao chúng ta lại gặp được nhau
+    Đó chắc là cái duyên
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không đâu bạn ạ liu liu :3',
-    question:'Bạn nghĩ sao về mối quan hệ này🥰',
+    btnNo: 'Không đâu liu liu :3',
+    question:'Bạn nghĩ sao về 1 mối quan hệ nghiêm túc',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối 8h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiiii lun <3',
-    messLink: 'hhttps://www.facebook.com/be.juno.1562' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/be.juno.1562' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
